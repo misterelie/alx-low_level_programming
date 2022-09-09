187b97d36066d2ab8959be02b704ba9d88a584d8
misterelie <elie.kouadio@uvci.edu.ci>
2022-09-08 20:26:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - this program prints exactly
+ * and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * followed by a new line to the standard error.
+ *
+ * Return: 1 for success
+ */
+int main(void) 
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
+
