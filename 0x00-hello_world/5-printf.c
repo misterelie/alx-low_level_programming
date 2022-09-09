41cd6f51ee4e51f5c3dd6b94bcd38b3667523e4b
misterelie <elie.kouadio@uvci.edu.ci>
2022-09-08 19:28:48 -0700

    Fix: Fix printf issue

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *  * main - This program uses the function printf to print
+ *
+ *   * exactly with proper grammar, but the outcome is a piece of art,
+ *
+ *    * followed by a newline.
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+int main(void)
+{
+       	printf("with proper, grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
