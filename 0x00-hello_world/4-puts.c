7a629b829971a9e5f27e4567f3eab82b65d2b495
misterelie <elie.kouadio@uvci.edu.ci>
2022-09-08 19:03:11 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - This program uses the function put to print
+ *
+ *   * exactly \"Programming is like building a multilingual puzzle,
+ *
+ *    * followed by a newline.
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
