1200ae25f196c2ad6c89183728a7c29ade91fcfe
misterelie <70035889+misterelie@users.noreply.github.com>
2022-09-09 02:05:14 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - This program uses the function put to print
+ * exactly \"Programming is like building a multilingual puzzle,
+ * followed by a newline.
  *
- *  * main - This program uses the function put to print
- *
- *   * exactly \"Programming is like building a multilingual puzzle,
- *
- *    * followed by a newline.
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
