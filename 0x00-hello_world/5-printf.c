3d5e387afb285567128e26803588a2c18fa196fb
misterelie <70035889+misterelie@users.noreply.github.com>
2022-09-09 02:30:22 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
+
 /**
+ * main - This program uses the function printf to print
+ * exactly with proper grammar, but the outcome is a piece of art,
+ * followed by a newline.
  *
- *  * main - This program uses the function printf to print
- *
- *   * exactly with proper grammar, but the outcome is a piece of art,
- *
- *    * followed by a newline.
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-       	printf("with proper, grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
