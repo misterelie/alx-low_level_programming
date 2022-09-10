a6b33bf30b5836e0b607d5157932d5683a007c69
misterelie <70035889+misterelie@users.noreply.github.com>
2022-09-10 04:54:37 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,10 @@
 /*
  * File: 100-print_comb3.c
+ * Auth: misterelie
  */
+
 #include <stdio.h>
+
 /**
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
@@ -11,7 +14,7 @@
 int main(void)
 {
 	int digit1, digit2;
-	
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -21,12 +24,13 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 }
