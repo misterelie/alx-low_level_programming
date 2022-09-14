1f02855df4705c868306c57045823e625c0e1ffa
misterelie <70035889+misterelie@users.noreply.github.com>
2022-09-14 05:04:03 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,22 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(',');
-		_putchar(' ');
+		_putchar('0');
 
-		prod = num * mult;
-
-		if (prod <= 9)
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((prod / 10) + '0');
 
-		_putchar((prod % 10) + '0');
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
