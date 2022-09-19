a784db986ad0af2c1649b0f6539a1556c8bfd787
misterelie <70035889+misterelie@users.noreply.github.com>
2022-09-19 19:00:39 +0000

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  * _strlen - returns the length of a string.
- *  @s: input string.
- *  Return: length of a string.
+ * @s: input string.
+ * Return: length of a string.
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int count = 0;
 
-	while(*(s + count) != '\0')
+	while (*(s + count) != '\0')
 		count++;
 	return (count);
 }
