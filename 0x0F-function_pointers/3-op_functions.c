4be6a0145b898e083c0e50c2b0b21a58ec95b090
misterelie <70035889+misterelie@users.noreply.github.com>
2022-10-13 13:32:36 +0000

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -17,11 +17,23 @@ int op_add(int a, int b)
  * @a: first number.
  * @b: second number.
  *
- * return: difference.
+ * Return: difference.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
 }
 
 /**
@@ -46,7 +58,7 @@ int op_div(int a, int b)
  * @a: first number.
  * @b: second number.
  *
- * Return: remainder of the division.[[I
+ * Return: remainder of the division.
  */
 int op_mod(int a, int b)
 {
