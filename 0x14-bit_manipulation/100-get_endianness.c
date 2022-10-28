aafc0cde344939b58e0672f7981f25bfa1bb9638
misterelie <70035889+misterelie@users.noreply.github.com>
2022-10-28 05:38:58 +0000

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,12 +3,12 @@
 /**
  * get_endianness - checks the endianness
  *
- * Return: 0 if big endian, 1 if little endianI[I
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	unsigned int x;
-	charc *c;
+	char *c;
 
 	x = 1;
 	c = (char *) &x;
